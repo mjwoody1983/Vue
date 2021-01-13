10347f27f7d83ab9232a08875e59e71d39fa97d7
mwoody1983 <mjwoodison@gmail.com>
2021-01-13 09:02:11 +0000

    Initial project commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <v-container
+        class="px-0"
+        fluid
+    >
+      <v-switch
+          v-model="listItems.switch"
+          :label="`Switch 1: ${listItems[0].switch.toString()}`"
+          @click="details(listItems[0].label, !listItems[0].switch)"
+      ></v-switch>
+
+      <v-data-table
+          :headers="headers"
+          :items="listItems"
+          class="elevation-2"
+        >
+
+
+              <template slot="item.switch" slot-scope="{ item }">
+                <v-switch
+                    v-model="item.switch"
+                    @click="details(item.label, item.switch)"
+                ></v-switch>
+              </template>
+
+              <template slot="item.x_t" slot-scope="{ item }">
+                <v-checkbox
+                    v-model="item.x_t"
+                    @click="details(item.label, item.x_t)"
+                ></v-checkbox>
+              </template>
+
+
+      </v-data-table>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data () {
+      return {
+        switch1: true,
+        listItems: [
+          {label: 0,switch: false, x_t: false},
+          {label: 1,switch: true, x_t: false},
+          {label: 2,switch: true, x_t: false}
+        ],
+        headers: [
+            {text: 'Label', sortable: false, value: 'label'},
+            {text: 'Switch', sortable: false, value: 'switch'},
+            {text: 'xt', sortable: false, value: 'x_t'}
+        ]
+      }
+    },
+    methods: {
+      details(i) {
+        //this.listItems[i].switch = itemUpdate
+        console.log('After...', this.listItems[i].x_t)
+      }
+    }
+  }
+</script>
