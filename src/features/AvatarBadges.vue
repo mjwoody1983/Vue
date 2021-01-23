ef43e56f5cd69f336d4db0aca5e8f008c540237e
mwoody1983 <mjwoodison@gmail.com>
2021-01-23 10:40:36 +0000

    Badges added to AvatarBadges route

--- src/features/AvatarBadges.vue
+++ src/features/AvatarBadges.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
     <v-app id="inspire">
+
+      <!-- Avatar section -->
       <v-container fluid>
         <v-row justify="center">
           <v-subheader>Today</v-subheader>
@@ -86,7 +88,69 @@
             </v-expansion-panel>
           </v-expansion-panels>
         </v-row>
+
+        <v-container>
+          <v-spacer></v-spacer>
+        </v-container>
+        <!-- Badges section -->
+        <v-container>
+          <v-row
+              align="center"
+              justify="center"
+          >
+            <v-badge
+                bordered
+                color="error"
+                icon="mdi-lock"
+                overlap
+            >
+              <v-btn
+                  class="white--text"
+                  color="error"
+                  depressed
+              >
+                Lock Account
+              </v-btn>
+            </v-badge>
+
+            <div class="mx-3"></div>
+
+            <v-badge
+                bordered
+                bottom
+                color="deep-purple accent-4"
+                dot
+                offset-x="10"
+                offset-y="10"
+            >
+              <v-avatar size="40">
+                <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+              </v-avatar>
+            </v-badge>
+
+            <div class="mx-3"></div>
+
+            <v-badge
+                avatar
+                bordered
+                overlap
+            >
+              <template v-slot:badge>
+                <v-avatar>
+                  <v-img src="https://cdn.vuetifyjs.com/images/logos/v.png"></v-img>
+                </v-avatar>
+              </template>
+
+              <v-avatar size="40">
+                <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+              </v-avatar>
+            </v-badge>
+          </v-row>
+        </v-container>
       </v-container>
+
+
+
     </v-app>
   </div>
 </template>
